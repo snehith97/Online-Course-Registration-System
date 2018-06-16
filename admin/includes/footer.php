0eaa41cb1ca2905c6feb0abfb8c828c9e7a59814
snehith97 <30771404+snehith97@users.noreply.github.com>
2018-06-17 01:40:15 +0530

    Add files via upload

---
+++ admin/includes/footer.php
@@ -0,0 +1,10 @@
+<footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    &copy; 2017 Online Course Registration | By : <a href="../index.php" target="_blank">Snehith</a>
+                </div>
+
+            </div>
+        </div>
+    </footer>
---
+++ admin/includes/header.php
@@ -0,0 +1,21 @@
+<?php
+error_reporting(0);
+?>
+
+    <!-- HEADER END-->
+    <div class="navbar navbar-inverse set-radius-zero">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                 <br>
+                  <img src="assets/img/logo1.png" alt="NIT Sikkim" height="119" width="640"><br>
+                  <a  style="color:#fff; font-size:35px;8px; line-height:24px;">Online Course Registration
+                  </a>
+  <br><br>
+            </div>
+            </div>
+        </div>
